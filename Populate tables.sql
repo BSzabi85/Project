bafeb67ea3c541fa685c87d1f3c49df357c51d9b
BSzabi85 <128228740+BSzabi85@users.noreply.github.com>
2023-05-22 09:51:12 +0300

    Update Populate tables.sql

--- Populate tables.sql
+++ Populate tables.sql
@@ -4,9 +4,11 @@ Go
 Exec uspAddType @TypeName = 'Database Admin';
 Exec uspAddType @TypeName = 'Administrator';
 Exec uspAddType @TypeName = 'Asistent social';
-Exec uspAddType @TypeName = 'Financiar';
+Exec uspAddType @TypeName = 'Financiar contabil';
 Exec uspAddType @TypeName = 'Medical';
 Exec uspAddType @TypeName = 'Terapeut';
 Exec uspAddType @TypeNamr = 'Ingrijitor';
 Exec uspAddType @TypeName = 'Bucatar';
 Exec uspAddType @TypeName = 'Personal curatenie';
+Exec uspAddType @TypeName = 'Personal intretinere';
+Exec uspAddType @TypeName = 'Portar';
