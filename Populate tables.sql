ba158a8456a14f1d9c4bd42e7eb9e3cc0e10bf57
BSzabi85 <128228740+BSzabi85@users.noreply.github.com>
2023-05-22 10:21:28 +0300

    Update Populate tables.sql

--- Populate tables.sql
+++ Populate tables.sql
@@ -4,15 +4,38 @@ Go
 RaisError ('Populating table "Person.Type",0 ,1) With NoWait;
 
 Exec uspAddType @TypeName = 'Database Admin';
+Go
+
 Exec uspAddType @TypeName = 'Administrator';
+Go
+
 Exec uspAddType @TypeName = 'Asistent social';
+Go
+
 Exec uspAddType @TypeName = 'Financiar contabil';
+Go
+
 Exec uspAddType @TypeName = 'Medical';
+Go
+
 Exec uspAddType @TypeName = 'Terapeut';
+Go
+
 Exec uspAddType @TypeNamr = 'Ingrijitor';
+Go
+
 Exec uspAddType @TypeName = 'Bucatar';
+Go
+
 Exec uspAddType @TypeName = 'Personal curatenie';
+Go
+
 Exec uspAddType @TypeName = 'Personal intretinere';
+Go
+
 Exec uspAddType @TypeName = 'Portar';
+Go
 
 RaisError ('Done.',0, 1) With NoWait;
+
+use master;
