8d6b296691313b8ed78130f2e956c6859b3b3036
BSzabi85 <128228740+BSzabi85@users.noreply.github.com>
2023-05-28 11:41:33 +0300

    Populate tables.sql

--- Populate tables.sql
+++ Populate tables.sql
@@ -33,7 +33,7 @@ Exec uspAddPerson
 	@FName = 'Banyai',
 	@LName = 'Szabolcs',
 	@PNN = '1234567890123',
-	@Aline1 = 'Str. Florilor ',
+	@Aline1 = 'Str. Florilor',
 	@City = 'Floresti',
 	@Prov = 'Cluj',
 	@PCode = '407280',
@@ -41,6 +41,21 @@ Exec uspAddPerson
 	@Email = 'b*******5@gmail.com',
 	@UserName = 'Bszabi85',
 	@PHash = 'TestPassword';
+
+Exec uspAddPerson
+@Ptype = 3,
+@FName = 'Banyai',
+@MName = 'Csilla',
+@LName = 'Andrea',
+@PNN = '2345678901234',
+@ALine1 = 'Str. Florilor',
+@City = 'Floresti',
+@Prov = 'Cluj',
+@PCode = '407280',
+@PhoneNo = '0040731452390',
+@Email = 'p_c***********a@yahoo.com',
+@UserName = 'Csilla85',
+@PHash = 'Testpass';
 	
 --Va genera eroare pentru ca @PNN nu este un CNP VALID. la mine pe calculator datele sunt corecte 
 
